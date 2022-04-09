93d72e936788aef17b66c097256dd059ffb95b20
charanmurugan <murugancharan245@gmail.com>
2022-04-09 11:23:51 +0530

    conitional statement

---
+++ IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String args[]) {
+        String name = "charan";
+        if (name == "Charan") {
+            System.out.print(name);
+        } else {
+            System.out.print("Invalid");
+        }
+
+    }
+}
